c8de0ca492fc30820f7183d13dcb20bd1df48910
YoanWaza <yoan.hagel96@gmail.com>
2024-07-20 15:46:19 +0300

    Add unit tests

---
+++ tests/dummy.spec.js
@@ -0,0 +1,11 @@
+import request from 'supertest';
+import app from '../src/index.js'; // Adjust the path if necessary
+
+describe('API routes', () => {
+  it('should return pong from /ping', (done) => {
+    request(app)
+      .get('/ping')
+      .expect(200)
+      .expect('pong <team’s number>', done);
+  });
+});
